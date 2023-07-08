6eaed4311138d8ffd810317c3901e84b131e52a1
Charlie Hay <charlie.hay@didomi.io>
2023-07-08 19:36:00 -0400

    feat(tde-00): update linking and fix constants warning

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,28 +1,37 @@
 import styled from 'styled-components/native'
 import { Link } from 'expo-router'
+import { openURL } from 'expo-linking'
 
-interface Props {
-  href: string
-  text: string
-}
+type Props = { href: string; text: string }
 
 export default function LinkButton({ href, text }: Props) {
-  return (
-    <S.Button testID="link-button" href={href}>
-      <S.ButtonText testID="link-button-text">{text}</S.ButtonText>
-    </S.Button>
+  return href.substring(0, 1) === '/' ? (
+    <S.InternalLink testID="link-button" href={href}>
+      <S.LinkText testID="link-button-text">{text}</S.LinkText>
+    </S.InternalLink>
+  ) : (
+    <S.ExternalLink testID="link-button" onPress={() => openURL(href)}>
+      <S.LinkText testID="link-button-text">{text}</S.LinkText>
+    </S.ExternalLink>
   )
 }
 
 const S = {
-  Button: styled(Link)`
+  ExternalLink: styled.TouchableOpacity`
     padding: ${p => p.theme.dimensions(10, 'px')} ${p => p.theme.dimensions(20, 'px')};
     border-color: ${p => p.theme.highlight};
     border-width: ${p => p.theme.dimensions(1, 'px')};
     border-radius: ${p => p.theme.dimensions(5, 'px')};
     background-color: transparent;
   `,
-  ButtonText: styled.Text`
+  InternalLink: styled(Link)`
+    padding: ${p => p.theme.dimensions(10, 'px')} ${p => p.theme.dimensions(20, 'px')};
+    border-color: ${p => p.theme.highlight};
+    border-width: ${p => p.theme.dimensions(1, 'px')};
+    border-radius: ${p => p.theme.dimensions(5, 'px')};
+    background-color: transparent;
+  `,
+  LinkText: styled.Text`
     color: ${p => p.theme.highlight};
     font-weight: 600;
   `
